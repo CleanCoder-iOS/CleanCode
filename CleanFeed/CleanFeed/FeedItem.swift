33fb87da758e66d6060ac48e63b44924f2cb58a7
Antriksh Verma <emailtoantriksh@gmail.com>
2025-10-20 17:26:46 -0700

    Create `FeedItem` model

---
+++ CleanFeed/CleanFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  CleanFeed
+//
+//  Created by Antriksh Verma on 10/20/25.
+//
+
+import Foundation
+
+struct FeedItem {
+    private let id: UUID
+    private let description: String?
+    private let location: String?
+    private let imageURL: URL
+}
